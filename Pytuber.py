ad46baaf00445391c99ca3d3d294d81845a14437
Shubh96 <subhamsarda@gmail.com>
2018-02-14 20:47:55 +0530

    Optimised the code used built-in exception handler by Pytube to validate video link from option 2

--- Pytuber.py
+++ Pytuber.py
@@ -1,6 +1,7 @@
 # ==================================import statements====================================
 
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 import requests
 from bs4 import BeautifulSoup
 import win32com.client as wincl
@@ -10,31 +11,27 @@ import re
 
 # ========================initializing the speaker voice and validation regex===========================
 speaker = wincl.Dispatch("SAPI.SpVoice")
-ytvideo_regex = '^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$'
-
 
 # ==================================function definition to download video==============================
 
+
 def video_download(download_url):
-    yt_object = YouTube(download_url)
-    dl_video_stream = yt_object.streams.first()
-    dl_video_stream.download(download_location)
+    try:
+        yt_object = YouTube(download_url)
+        dl_video_stream = yt_object.streams.first()
+
+        speaker.Speak("Downloading Video")
+        dl_video_stream.download(download_location)
+    except RegexMatchError:
+        take_input()
 
 
-# ==================================function definition to validate video==============================
-
-def validate_video_url(video_link):
-    matched = re.search(ytvideo_regex, video_link)
-
-    if matched is None:
-        speaker.Speak("Invalid URL! Please enter a YouTube video URL")
-        video_link = input('Enter the video link: ')
-        return validate_video_url(video_link)
-    else:
-        return video_link
-
-
+def take_input():
+    speaker.Speak("Invalid URL! Please enter a YouTube video link")
+    video_link = input('Enter the video link: ')
+    video_download(video_link)
 # ===========================function definition to generate playlist by web scraping==========================
+
 
 def playlist_generate():
     html_doc = requests.get(playlist_link_input).text
@@ -44,9 +41,15 @@ def playlist_generate():
     if len(playlist_anchor) == 0:
         playlist_anchor = parsed_doc('a', {'class': 'playlist-video'})
 
+    return playlist_anchor
+
+# ===========================function definition to create playlist by web scraping==========================
+
+
+def create_playlist():
     playlist = []
 
-    for pla in playlist_anchor:
+    for pla in pl_anchor:
         playlist.append('https://www.youtube.com' + pla.get('href'))
 
     length = len(playlist)
@@ -79,24 +82,29 @@ userMenuChoice = int(input("Enter your choice: "))
 
 if userMenuChoice == 1:
 
+    speaker.Speak("Select download location")
+    download_location = filedialog.askdirectory()
+
     speaker.Speak("Enter the playlist link")
     playlist_link_input = input('Enter the playlist link: ')
 
-    speaker.Speak("Select download location")
-    download_location = filedialog.askdirectory()
+    pl_anchor = playlist_generate()
 
-    playlist_generate()
+    while len(pl_anchor) == 0:
+        speaker.Speak("Invalid URL! Please enter a YouTube playlist")
+        playlist_link_input = input('Enter the playlist link: ')
+        pl_anchor = playlist_generate()
+    else:
+        create_playlist()
 
 elif userMenuChoice == 2:
 
-    speaker.Speak("Enter the video link")
-    video_link_input = input('Enter the video link: ')
-    video_link_input = validate_video_url(video_link_input)
-
     speaker.Speak("Select download location")
     download_location = filedialog.askdirectory()
 
-    speaker.Speak("Downloading Video")
+    speaker.Speak("Enter the video link")
+    video_link_input = input('Enter the video link: ')
+
     video_download(video_link_input)
 
     speaker.Speak("Video downloaded")
